062c2f24dcbb36a007fa07a6de6df155454b8dd8
Rodger Higgins <rdhiggins@gmail.com>
2014-01-25 11:41:38 -0600

    Moved prompt strings to there own scripts

---
+++ zsh/prompt.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+autoload -U colors
+colors
+setopt PROMPT_SUBST ; PS1='[%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%m%{$reset_color%} %{$fg[cyan]%}%c$(__git_ps1 " (%s)")%{$reset_color%}]
+\$ ' 
