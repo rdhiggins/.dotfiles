b197c53604108d064ed703ddcc1ee606d472d8d3
Rodger Higgins <rdhiggins@gmail.com>
2014-01-25 11:57:25 -0600

    Added in git config

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
-ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
 
-ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+function link_file {
+	[[ -s $2 ]] &&rm $2
+	ln -s $1 $2 
+}
 
-ln -s ~/.dotfiles/bin ~/bin
+
+# Bash config
+link_file ~/.dotfiles/bash/bashrc ~/.bashrc
+link_file ~/.dotfiles/bash/bash_profile ~/.bash_profile
+
+# Zsh config
+link_file ~/.dotfiles/zsh/zshrc ~/.zshrc
+
+# Bin Directory
+link_file ~/.dotfiles/bin ~/bin
+
+# Git Config
+link_file ~/.dotfiles/git/gitconfig ~/.gitconfig
+link_file ~/.dotfiles/git/gitignore_global ~/.gitignore_global
