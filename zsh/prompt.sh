c2a856c08d5785b2bd5cfd2836561c2362a9d783
Rodger Higgins <rdhiggins@gmail.com>
2014-02-06 18:01:04 -0600

    Revised prompt

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -2,5 +2,4 @@
 
 autoload -U colors
 colors
-setopt PROMPT_SUBST ; PS1='[%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%m%{$reset_color%} %{$fg[cyan]%}%c$(__git_ps1 " (%s)")%{$reset_color%}]
-\$ ' 
+setopt PROMPT_SUBST ; PS1='[%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%m%{$reset_color%} %{$fg[cyan]%}%c$(__git_ps1 " (%s)")%{$reset_color%}]\$ ' 
