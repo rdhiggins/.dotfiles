6850c3b0ff7c80c65b5018017ae7797d5938cba0
Rodger Higgins <rdhiggins@gmail.com>
2019-12-11 11:35:11 -0600

    Some twiddles

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -3,8 +3,8 @@
 autoload -U colors
 colors
 
-setopt PROMPT_SUBST ; PS1='[%{$fg[magenta]%}%n%{$reset_color%}@%{$fg_bold[green]%}%m%{$reset_color%} %{$fg_bold[yellow]%}%~%{$reset_color%}%{$fg_bold[cyan]%}$(__git_ps1 " (%s)")%{$reset_color%}]
-\$ ' 
+setopt PROMPT_SUBST ; PS1='[%{$fg[magenta]%}%n%{$reset_color%}@%{$fg_bold[green]%}%m%{$reset_color%} %{$fg_bold[yellow]%}%~%{$reset_color%}]
+[%{$fg_bold[cyan]%}%*%{$reset_color%}%{$fg_bold[red]%}$(__git_ps1 " (%s)")%{$reset_color%}] \$ '
 
 
-# RPROMPT='$(prompt_git_info)'
+#RPROMPT='%{$fg_bold[red]%}$(__git_ps1 " (%s)")%{$reset_color%}'
